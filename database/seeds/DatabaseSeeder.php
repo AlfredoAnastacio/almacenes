2d232136ccfdb69f6a0430930bf6f86104450814
AlfredoR <fredy.anastacio92@gmail.com>
2021-04-08 07:38:25 -0500

    fakers para tablas cat_productos, cat_almacenes y existencias

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker::create();
+        foreach (range(1,15) as $index) {
+            DB::table('cat_productos')->insert([
+	            'sku' => $faker->numberBetween('100000000', '200000000'),
+	            'descripcion' => $faker->word,
+	            'marca' => $faker->randomElement(['BIMBO', 'PEPSI', 'LALA', 'GAMESA']),
+	            'color' => $faker->randomElement(['rojo', 'morado', 'verde', 'azul', 'blanco']),
+	            'precio' => $faker->randomElement(['100', '200', '300', '400'])
+	        ]);
+	    }
+        foreach (range(1,4) as $index) {
+	        DB::table('cat_almacenes')->insert([
+	            'nombre_almacen' => $faker->cityPrefix,
+	            'localizacion' => $faker->state,
+	            'responsable' => $faker->name,
+	            'tipo' => $faker->randomElement(['VIRTUAL', 'FISICO']),
+	        ]);
+	    }
+        foreach (range(1,20) as $index) {
+	        DB::table('existencias')->insert([
+	            'id_producto' => $faker->numberBetween('1', '15'),
+	            'id_almacen' => $faker->numberBetween('1', '4'),
+	            'existencias' => $faker->numberBetween('10', '100'),
+	        ]);
+	    }
     }
 }
